a809cec6578403305a0d0954c811dceced35d373
Lorenz Hörburger <e12024737@student.tuwien.ac.at>
2022-12-02 14:36:06 +0100

    add SessionUtil & refactore security

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/CompetitionServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/CompetitionServiceImpl.java
@@ -3,8 +3,10 @@ package at.ac.tuwien.sepm.groupphase.backend.service.impl;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.CompetitionDetailDto;
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.mapper.CompetitionMapper;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Competition;
+import at.ac.tuwien.sepm.groupphase.backend.exception.ForbiddenException;
 import at.ac.tuwien.sepm.groupphase.backend.repository.CompetitionRepository;
 import at.ac.tuwien.sepm.groupphase.backend.service.CompetitionService;
+import at.ac.tuwien.sepm.groupphase.backend.util.SessionUtils;
 import at.ac.tuwien.sepm.groupphase.backend.validation.CompetitionValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,20 +21,25 @@ public class CompetitionServiceImpl implements CompetitionService {
     private final CompetitionRepository competitionRepository;
     private final CompetitionMapper competitionMapper;
     private final CompetitionValidator competitionValidator;
+    private final SessionUtils sessionUtils;
 
-    public CompetitionServiceImpl(CompetitionRepository competitionRepository, CompetitionMapper competitionMapper, CompetitionValidator competitionValidator) {
+    public CompetitionServiceImpl(CompetitionRepository competitionRepository, CompetitionMapper competitionMapper, CompetitionValidator competitionValidator, SessionUtils sessionUtils) {
         this.competitionRepository = competitionRepository;
         this.competitionMapper = competitionMapper;
         this.competitionValidator = competitionValidator;
+        this.sessionUtils = sessionUtils;
     }
 
     @Override
     public CompetitionDetailDto create(CompetitionDetailDto competitionDetailDto) {
         LOGGER.debug("Create competition {}", competitionDetailDto);
-        //ToDo: Check user
+        if (!sessionUtils.isCompetitionManager()) {
+            throw new ForbiddenException("No Permission to create a competition");
+        }
         competitionValidator.validate(competitionDetailDto);
         Competition competition = competitionMapper
             .competitionDetailDtoToCompetition(competitionDetailDto);
+        competition.setCreator(sessionUtils.getSessionUser());
         return competitionMapper
             .competitionToCompetitionDetailDto(competitionRepository.save(competition));
     }

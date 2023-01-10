d42ee4c336c2241229ee78b66d8abd0f4ed3e0f9
Artem Chornyi <e11922295@student.tuwien.ac.at>
2023-01-10 20:31:14 +0000

    Remove unneccessary code

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/validation/CompetitionValidator.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/validation/CompetitionValidator.java
@@ -39,40 +39,6 @@ public class CompetitionValidator extends Validator<CompetitionDetailDto> {
 
     @Override
     protected void validateCustom(CompetitionDetailDto toValidate) {
-    }
 
-    public void validateSelfRegisterToCompetition(Long competitionId, Long groupPreference) {
-        if (competitionId == null) {
-            throw new ValidationListException("Validation error when self register to competition", List.of("Competition id must be given"));
-        }
-
-        competitionRepository.findById(competitionId)
-            .orElseThrow(() -> new NotFoundException("No competition with id " + competitionId));
-
-        if (groupPreference != null && !isCompetitionGroup(competitionId, groupPreference)) {
-            throw new ConflictException(
-                "Conflict when registering to competition",
-                List.of("Unknown group " + groupPreference + " for competition " + competitionId));
-        }
-    }
-
-    public void validateParticipantRegistration(Long competitionId, ParticipantRegistrationDto registrationDto) {
-        participantRegistrationValidator.validateAnnotations(registrationDto);
-        List<String> conflictErrors = new ArrayList<>();
-        Optional<Competition> competition = competitionRepository.findById(competitionId);
-        Optional<ApplicationUser> applicationUser = applicationUserRepository.findById(registrationDto.getUserId());
-
-        if (competition.isEmpty()) {
-            throw new NotFoundException("Competition with id " + competitionId + " not found.");
-        }
-
-        if (applicationUser.isEmpty()) {
-            throw new NotFoundException("Participant with id " + registrationDto.getUserId() + " not found.");
-        }
-        throw new ConflictException("Conflicts while register participant", conflictErrors);
-    }
-
-    private boolean isCompetitionGroup(Long competitionId, Long groupId) {
-        return gradingGroupRepository.findByIdAndCompetitionId(groupId, competitionId).isPresent();
     }
 }
